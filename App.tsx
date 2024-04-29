7e3f0f236e492eca34adbc6e721fccb0944eca09
Rashmi Sharmila Fernando <rsrashi0430@gmail.com>
2024-04-29 02:11:19 -0700

    The send message page has been created.

--- App.tsx
+++ App.tsx
@@ -1,35 +1,116 @@
-import { Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TouchableHighlight, View } from "react-native";
 import React, { useState } from 'react';
-
-
+import { Appbar } from "react-native-paper";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faArrowLeft, faArrowRight, faChevronRight, faTruckArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 
 const App = () => {
 
-  const handleButtonPress = (buttonName: string) => {
-    console.log(`Button ${buttonName} pressed`);
-    // Add your logic for each button here
+  const handlePress = (item: { id: string; key: string; }) => {
+    // Do something when the item is pressed
+    console.log("Item pressed:", item);
   };
 
+
+  const [Id, setIds] = useState([
+    { id: 'xxx-xxx-xxx', key: '1' },
+    { id: 'xxx-xxx-xxx', key: '2' },
+    { id: 'xxx-xxx-xxx', key: '3' },
+    { id: 'xxx-xxx-xxx', key: '4' },
+    { id: 'xxx-xxx-xxx', key: '5' },
+    { id: 'xxx-xxx-xxx', key: '6' },
+    { id: 'xxx-xxx-xxx', key: '7' },
+    { id: 'xxx-xxx-xxx', key: '8' },
+    { id: 'xxx-xxx-xxx', key: '9' },
+    { id: 'xxx-xxx-xxx', key: '10' },
+    { id: 'xxx-xxx-xxx', key: '11' },
+    { id: 'xxx-xxx-xxx', key: '12' },
+
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Send Messages</Text>
-      
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+
+        <View style={styles.header}>
+          <FontAwesomeIcon icon={faArrowLeft} style={styles.backIcon} />
+          <Text style={styles.title}>Send Messages</Text>
+        </View>
+
+
+      </View>
+      <View style={styles.section}>
+        <ScrollView >
+          {
+            Id.map(item => (
+              <TouchableHighlight
+                key={item.key}
+                onPress={() => handlePress(item)} // handlePress function to handle touch events
+                underlayColor="transparent" // Customize the color when touched
+              >
+                <View style={styles.itemContainer}>
+                  <Text style={styles.item}>{item.id}</Text>
+                  <FontAwesomeIcon icon={faArrowRight} style={styles.icon} />
+                </View>
+              </TouchableHighlight>
+            ))
+          }
+        </ScrollView>
+      </View>
+
+    </SafeAreaProvider>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: 'white',
     paddingTop: 30,
+  },
+  header:{
+    
+    flexDirection:'row',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    textAlign:'center',
+    textAlign: 'center',
     color: 'black',
+    marginLeft: 120,
+    
+
+  },
+  item: {
+    marginTop: 5,
+    padding: 10,
+    color: 'black',
+    fontSize: 20,
+  },
+  section: {
+    flex: 9,
+    backgroundColor: '#ffffff',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
   },
 
+  icon: {
+    fontSize: 30,
+    color: '#1B8301',
+  },
+  backIcon:{
+    fontSize: 30,
+    color: '#000000',
+    marginLeft:10,
+    marginTop:5
+  }
 });
 export default App;
--- pages/SendMessage.tsx
+++ pages/SendMessage.tsx
@@ -1,89 +1,116 @@
-import { Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TouchableHighlight, View } from "react-native";
 import React, { useState } from 'react';
-
-
+import { Appbar } from "react-native-paper";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faArrowLeft, faArrowRight, faChevronRight, faTruckArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 
 const SendMessage = () => {
 
-  const handleButtonPress = (buttonName: string) => {
-    console.log(`Button ${buttonName} pressed`);
-    // Add your logic for each button here
+  const handlePress = (item: { id: string; key: string; }) => {
+    // Do something when the item is pressed
+    console.log("Item pressed:", item);
   };
 
+
+  const [Id, setIds] = useState([
+    { id: 'xxx-xxx-xxx', key: '1' },
+    { id: 'xxx-xxx-xxx', key: '2' },
+    { id: 'xxx-xxx-xxx', key: '3' },
+    { id: 'xxx-xxx-xxx', key: '4' },
+    { id: 'xxx-xxx-xxx', key: '5' },
+    { id: 'xxx-xxx-xxx', key: '6' },
+    { id: 'xxx-xxx-xxx', key: '7' },
+    { id: 'xxx-xxx-xxx', key: '8' },
+    { id: 'xxx-xxx-xxx', key: '9' },
+    { id: 'xxx-xxx-xxx', key: '10' },
+    { id: 'xxx-xxx-xxx', key: '11' },
+    { id: 'xxx-xxx-xxx', key: '12' },
+
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>APEX</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('1')}>
-          <Image source={require('../AwesomeProject/images/send.png')} style={styles.buttonImage} />
-          <Text style={styles.buttonText}>Send</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('2')}>
-        <Image source={require('../AwesomeProject/images/recevied.png')} style={styles.buttonImage} />
-          <Text style={styles.buttonText}>Received</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('3')}>
-        <Image source={require('../AwesomeProject/images/setting.png')} style={styles.buttonImage} />
-          <Text style={styles.buttonText}>Setting</Text>
-        </TouchableOpacity>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+
+        <View style={styles.header}>
+          <FontAwesomeIcon icon={faArrowLeft} style={styles.backIcon} />
+          <Text style={styles.title}>Send Messages</Text>
+        </View>
+
+
       </View>
-      <View style={styles.logoContainer}>
-        <Image source={require('../AwesomeProject/images/sltLogo.png')} style={styles.logoImage} />
+      <View style={styles.section}>
+        <ScrollView >
+          {
+            Id.map(item => (
+              <TouchableHighlight
+                key={item.key}
+                onPress={() => handlePress(item)} // handlePress function to handle touch events
+                underlayColor="transparent" // Customize the color when touched
+              >
+                <View style={styles.itemContainer}>
+                  <Text style={styles.item}>{item.id}</Text>
+                  <FontAwesomeIcon icon={faArrowRight} style={styles.icon} />
+                </View>
+              </TouchableHighlight>
+            ))
+          }
+        </ScrollView>
       </View>
-    </View>
+
+    </SafeAreaProvider>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: 'white',
     paddingTop: 30,
   },
+  header:{
+    
+    flexDirection:'row',
+  },
   title: {
-    fontSize: 40,
+    fontSize: 20,
     fontWeight: 'bold',
-    paddingLeft: 20,
-    paddingRight: 20,
+    textAlign: 'center',
     color: 'black',
+    marginLeft: 120,
+    
+
   },
-  buttonContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    marginTop: 100,
+  item: {
+    marginTop: 5,
+    padding: 10,
+    color: 'black',
+    fontSize: 20,
   },
-  button: {
-    width: 300,
-    height: 100,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 10,
+  section: {
+    flex: 9,
+    backgroundColor: '#ffffff',
+  },
+  itemContainer: {
     flexDirection: 'row',
-    borderRadius: 2,
-    elevation: 5,
-  },
-  buttonText: {
-    color: 'black',
-    fontSize: 18,
-    marginLeft: 10,
-  },
-  buttonImage: {
-    width: 90,
-    height: 90,
-    marginRight:100,
-  },
-  logoContainer: {
-    position: 'absolute',
-    bottom: 20,
-    left: 0,
-    right: 0,
+    justifyContent: 'space-between',
     alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
   },
-  logoImage: {
-    width: 150,
-    height: 50,
+
+  icon: {
+    fontSize: 30,
+    color: '#1B8301',
   },
+  backIcon:{
+    fontSize: 30,
+    color: '#000000',
+    marginLeft:10,
+    marginTop:5
+  }
 });
 export default SendMessage;
