58c42ab1afdafb5df3d642397af9238c30ffd42e
Pamindu Nawodya <pamidunawodya@gmail.com>
2024-04-29 22:13:54 +0530

    updated network page

--- App.tsx
+++ App.tsx
@@ -1,35 +1,18 @@
 import { Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import React, { useState } from 'react';
+const logo= require("./images/oops.png")
 
 
 
 
 const App = () => {
 
-  const handleButtonPress = (buttonName: string) => {
-    console.log(`Button ${buttonName} pressed`);
-    // Add your logic for each button here
-  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Send Messages</Text>
-      
+    <View style={{alignItems:'center'}}>
+    <Text style={{fontSize:30,color:'red'}} >Hello Guys</Text>
     </View>
   );
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    paddingTop: 30,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign:'center',
-    color: 'black',
-  },
+};
 
-});
 export default App;
