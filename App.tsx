370082b6812f8aca7f8b580ba26e2ed09302a7b7
Rashmi Sharmila Fernando <rsrashi0430@gmail.com>
2024-05-02 13:27:45 +0530

    Created EditId page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { ScrollView, StyleSheet, Text, TouchableHighlight, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, View } from "react-native";
 import React, { useState } from 'react';
-import { Appbar } from "react-native-paper";
+import { Appbar, Button } from "react-native-paper";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faArrowLeft, faArrowRight, faChevronRight, faTruckArrowRight } from '@fortawesome/free-solid-svg-icons';
@@ -36,28 +36,30 @@ const App = () => {
 
         <View style={styles.header}>
           <FontAwesomeIcon icon={faArrowLeft} style={styles.backIcon} />
-          <Text style={styles.title}>Send Messages</Text>
+          <Text style={styles.title}>Edit ID</Text>
         </View>
 
 
       </View>
       <View style={styles.section}>
-        <ScrollView >
-          {
-            Id.map(item => (
-              <TouchableHighlight
-                key={item.key}
-                onPress={() => handlePress(item)} // handlePress function to handle touch events
-                underlayColor="transparent" // Customize the color when touched
-              >
-                <View style={styles.itemContainer}>
-                  <Text style={styles.item}>{item.id}</Text>
-                  <FontAwesomeIcon icon={faArrowRight} style={styles.icon} />
-                </View>
-              </TouchableHighlight>
-            ))
-          }
-        </ScrollView>
+        <View style={styles.imageContainer}>
+          <Image source={require('../AwesomeProject/images/editId.png')} style={styles.buttonImage} />
+        </View>
+        <TextInput style={styles.input}
+          placeholder=" "
+
+        />
+        <Button 
+          style={styles. editButton}
+        >
+          <Text style={styles.addButtonText}>Edit</Text>
+        </Button >
+
+        <Button 
+          style={styles.cancleButton}
+        >
+          <Text style={styles.addButtonText}>Cancle </Text>
+        </Button >
       </View>
 
     </SafeAreaProvider>
@@ -69,17 +71,17 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     paddingTop: 30,
   },
-  header:{
-    
-    flexDirection:'row',
+  header: {
+
+    flexDirection: 'row',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
     color: 'black',
-    marginLeft: 120,
-    
+    marginLeft: 150,
+
 
   },
   item: {
@@ -106,11 +108,61 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: '#1B8301',
   },
-  backIcon:{
+  backIcon: {
     fontSize: 30,
     color: '#000000',
-    marginLeft:10,
-    marginTop:5
+    marginLeft: 10,
+    marginTop: 5
+  },
+  buttonImage: {
+    width: 200,
+    height: 200,
+  },
+  imageContainer: {
+    alignItems: 'center', // Center horizontally
+  },
+  input: {
+    borderWidth: 3,
+    borderColor: "black",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    fontSize: 18,
+    color: 'black',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10
+  },
+  editButton: {
+    width: 100,
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    position: 'absolute',
+    margin: 16,
+    right: 120,
+    top: 250,
+    marginTop: 30
+  },
+  addButtonText: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    color: 'white'
+  },
+  cancleButton: {
+    width: 100,
+  backgroundColor: 'black',
+  padding: 10,
+  borderRadius: 5,
+  marginBottom: 10,
+  position: 'absolute',
+  margin: 16,
+  right: 0,
+  top: 250,
+  marginTop: 30, 
+   
   }
 });
 export default App;
--- pages/EditIdPage.tsx
+++ pages/EditIdPage.tsx
@@ -1,50 +1,125 @@
-import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, View } from "react-native";
 import React, { useState } from 'react';
+import { Appbar, Button } from "react-native-paper";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faArrowLeft, faArrowRight, faChevronRight, faTruckArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 
+const EditIdPage = () => {
+
+  const handlePress = (item: { id: string; key: string; }) => {
+    // Do something when the item is pressed
+    console.log("Item pressed:", item);
+  };
 
 
-const NewMessage = () => {
+  const [Id, setIds] = useState([
+    { id: 'xxx-xxx-xxx', key: '1' },
+    { id: 'xxx-xxx-xxx', key: '2' },
+    { id: 'xxx-xxx-xxx', key: '3' },
+    { id: 'xxx-xxx-xxx', key: '4' },
+    { id: 'xxx-xxx-xxx', key: '5' },
+    { id: 'xxx-xxx-xxx', key: '6' },
+    { id: 'xxx-xxx-xxx', key: '7' },
+    { id: 'xxx-xxx-xxx', key: '8' },
+    { id: 'xxx-xxx-xxx', key: '9' },
+    { id: 'xxx-xxx-xxx', key: '10' },
+    { id: 'xxx-xxx-xxx', key: '11' },
+    { id: 'xxx-xxx-xxx', key: '12' },
 
+  ]);
 
   return (
+    <SafeAreaProvider>
+      <View style={styles.container}>
 
-    <View style={styles.container}>
-      <Text style={styles.title}>New Message</Text>
-
-      <Text style={styles.Subtext}>Id : xxx-xxx-xxx</Text>
-      <TextInput style={styles.input} placeholder="Enter Message" />
-
-      <TouchableOpacity
-        style={styles.addButton} >
-        <Text style={styles.addButtonText}></Text>
-      </TouchableOpacity>
+        <View style={styles.header}>
+          <FontAwesomeIcon icon={faArrowLeft} style={styles.backIcon} />
+          <Text style={styles.title}>Edit ID</Text>
+        </View>
 
 
-    </View>
+      </View>
+      <View style={styles.section}>
+        <View style={styles.imageContainer}>
+          <Image source={require('../AwesomeProject/images/editId.png')} style={styles.buttonImage} />
+        </View>
+        <TextInput style={styles.input}
+          placeholder=" "
 
+        />
+        <Button 
+          style={styles. editButton}
+        >
+          <Text style={styles.addButtonText}>Edit</Text>
+        </Button >
+
+        <Button 
+          style={styles.cancleButton}
+        >
+          <Text style={styles.addButtonText}>Cancle </Text>
+        </Button >
+      </View>
+
+    </SafeAreaProvider>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 25,
-    marginTop: 10,
-    backgroundColor: "white",
+    backgroundColor: 'white',
+    paddingTop: 30,
+  },
+  header: {
+
+    flexDirection: 'row',
   },
   title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-    color: "black",
-    textAlign: "center"
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'black',
+    marginLeft: 150,
+
+
+  },
+  item: {
+    marginTop: 5,
+    padding: 10,
+    color: 'black',
+    fontSize: 20,
+  },
+  section: {
+    flex: 9,
+    backgroundColor: '#ffffff',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
   },
 
-  Subtext: {
-    padding: 10,
-    marginBottom: 10,
-    fontSize: 20,
-    color: "black"
+  icon: {
+    fontSize: 30,
+    color: '#1B8301',
+  },
+  backIcon: {
+    fontSize: 30,
+    color: '#000000',
+    marginLeft: 10,
+    marginTop: 5
+  },
+  buttonImage: {
+    width: 200,
+    height: 200,
+  },
+  imageContainer: {
+    alignItems: 'center', // Center horizontally
   },
   input: {
     borderWidth: 3,
@@ -53,29 +128,41 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 10,
     fontSize: 18,
-    color: 'black'
+    color: 'black',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10
   },
-  task: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 15,
-    color: 'black'
-  },
-  addButton: {
+  editButton: {
+    width: 100,
     backgroundColor: 'black',
     padding: 10,
     borderRadius: 5,
     marginBottom: 10,
-
-
+    position: 'absolute',
+    margin: 16,
+    right: 120,
+    top: 250,
+    marginTop: 30
   },
   addButtonText: {
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 18,
     color: 'white'
-
   },
+  cancleButton: {
+    width: 100,
+  backgroundColor: 'black',
+  padding: 10,
+  borderRadius: 5,
+  marginBottom: 10,
+  position: 'absolute',
+  margin: 16,
+  right: 0,
+  top: 250,
+  marginTop: 30, 
+   
+  }
 });
-export default NewMessage;
+export default EditIdPage;
